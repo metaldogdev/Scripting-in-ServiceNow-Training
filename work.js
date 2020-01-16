f4561f5e428b596d5606c735449e6312f3039643
Lili Ivany <metaldog.dev@gmail.com>
2020-01-16 15:13:20 +0100

    add lab 2.2

--- work.js
+++ work.js
@@ -62,3 +62,21 @@ function onCellEdit(sysIDs, table, oldValues, newValue, callback) {
     callback(saveAndClose);
 }
 
+// Lab  2.2
+
+function onSubmit() {
+    if(g_form.getValue('impact') == 1 && g_form.getValue('urgency') == 1 && !g_user.hasRoleExactly('major_inc_mgr')) {
+
+        var ans = confirm("The customer is notified of all Priority-1 Incidents. Conform base information is included before submitting this P1 incident.\n\nSelect OK to submit, or Cancel to return to the record.");
+
+        if(!ans) {
+            g_form.addInfoMessage("Incident not submitted");
+            g_form.addInfoMessage("If base information is unavailable, use the 'Additional comments' field to document why it is missing.");
+            g_form.showFieldMsg('category', "Major incident base field");
+            g_form.showFieldMsg('cmdb_ci', "Major Incident base field");
+            g_form.showFieldMsg('assignment_group', "Major Incident base field");
+            g_form.showFieldMsg('short_description', "Major Incident base field");
+        }
+        return ans;
+    }
+}
